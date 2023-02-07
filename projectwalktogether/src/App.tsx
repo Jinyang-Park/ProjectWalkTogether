ae5e189ff805d7c63e8b1b902e3fc2aa098fa7fb
rhsok823 <rhsok823@gmail.com>
2023-02-07 23:25:03 +0900

    채팅페이지 완

--- projectwalktogether/src/App.tsx
+++ projectwalktogether/src/App.tsx
@@ -1,20 +1,7 @@
 import React from "react";
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
-import PostPage from "./pages/PostPage";
-
+import Router from "./routes/router";
 function App() {
-    return (
-        <div>
-            <PostPage></PostPage>
-        </div>
-    );
-=======
-import Router from "./routes/Router";
-function App() {
-  return <Router />;
->>>>>>> c7e71b9f16fbce7ee779acc34ca2673d3692b337
+    return <Router />;
 }
 
 export default App;
