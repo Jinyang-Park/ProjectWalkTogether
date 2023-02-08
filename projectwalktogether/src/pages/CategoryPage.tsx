abce11740ebdeb6be11b9a24620cd9a60dbff49c
Jinyang-park <dbsskdud60@gmail.com>
2023-02-08 16:40:59 +0900

    [feat] 카테고리 페이지, 디테일 페이지 수정

--- projectwalktogether/src/pages/CategoryPage.tsx
+++ projectwalktogether/src/pages/CategoryPage.tsx
@@ -31,30 +31,3 @@ export const CategoryTitle = styled.h1`
   letter-spacing: -0.64px;
   margin-bottom: 3rem;
 `;
-export const CategoryWrapper = styled.div`
-  display: flex;
-  grid-gap: 0.75rem 0;
-  gap: 1rem 0;
-  flex-wrap: wrap;
-  justify-content: flex-start;
-`;
-export const Categoryitem = styled.div`
-  cursor: pointer;
-  display: inline-flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  /* grid-gap: 0.125rem; */
-  gap: 0.7rem;
-  /* margin-left: 1rem; */
-  width: 10rem;
-`;
-export const Img = styled.img`
-  width: 5.25rem;
-  height: 5.25rem;
-  border-radius: 50%;
-`;
-export const ImgTitle = styled.p`
-  color: #6b6766;
-  font-weight: 700;
-`;
