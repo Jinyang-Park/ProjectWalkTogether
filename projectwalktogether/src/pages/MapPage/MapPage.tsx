c12d7629d286e1588ac63a9a3fdbc89d76d90c6d
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-08 16:29:43 +0900

    [docs] : 하위폴더 세분화 MapPage Only

--- projectwalktogether/src/pages/MapPage/MapPage.tsx
+++ projectwalktogether/src/pages/MapPage/MapPage.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import styled from 'styled-components'
 import * as S from './MapPage.style'
+import InfoList from './InfoList/InfoList'
+
+const { kakao } = window
+
+declare global {
+    interface Window {
+        kakao: any
+    }
+}
 
 const MapPage = () => {
     return (
         <S.MapPageContainer>
             <S.MapPageHeader>
+                <S.EmptyBox></S.EmptyBox>
                 <S.MapPageTitle>같이 걷고 싶은 사람들</S.MapPageTitle>
                 <S.LineMarker></S.LineMarker>
             </S.MapPageHeader>
-            <S.MapKaKaoMapContainer></S.MapKaKaoMapContainer>
-            <S.UserInfoContainer></S.UserInfoContainer>
+            <S.MapPageContentsWrapper>
+                <S.MapKaKaoMapContainer></S.MapKaKaoMapContainer>
+                <S.UserInfoContainer>
+                    <InfoList />
+                </S.UserInfoContainer>
+            </S.MapPageContentsWrapper>
         </S.MapPageContainer>
     )
 }
