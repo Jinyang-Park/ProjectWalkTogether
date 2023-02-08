d43c64d30a1e3a061b49bc422f2b5d1b0142e8ec
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-08 10:19:02 +0900

    [docs] : 불필요한 코드 삭제

--- projectwalktogether/src/pages/MapPage/InfoWrapper/ResultItem/ResultItem.style.tsx
+++ projectwalktogether/src/pages/MapPage/InfoWrapper/ResultItem/ResultItem.style.tsx
@@ -1,51 +1,7 @@
-import styled from 'styled-components'
-import {
-    BLACK_COLOR,
-    DARK_GRAY_COLOR,
-    PINK_COLOR,
-} from '../../../../common/colors'
+import React from 'react'
 
-export const Container = styled.div`
-    width: 100%;
-    border-bottom: 1px solid ${BLACK_COLOR};
-    padding: 1rem 0;
-    cursor: pointer;
-`
+const EmptyPage = () => {
+    return <div></div>
+}
 
-export const NameRow = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    padding-bottom: 1rem;
-`
-
-export const Name = styled.span`
-    height: 0.8rem;
-    margin-right: 0.5rem;
-    transition: 80ms ease-in-out;
-    &:hover {
-        color: ${PINK_COLOR};
-    }
-`
-
-export const IconsContainer = styled.div`
-    display: flex;
-    align-items: center;
-`
-
-export const Category = styled.div`
-    font-size: 0.9rem;
-    border-radius: 6px;
-    border: 1px solid ${BLACK_COLOR};
-    padding: 0.2rem 0.5rem;
-`
-
-export const Address = styled.div`
-    padding-bottom: 1rem;
-    font-size: 0.9rem;
-`
-
-export const Description = styled.div`
-    font-size: 0.9rem;
-    color: ${DARK_GRAY_COLOR};
-`
+export default EmptyPage
