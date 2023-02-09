a5e5cf9d4915a5cf82a5531be74ba4df90125a0c
Leekee01 <zeje23@naver.com>
2023-02-09 23:48:39 +0900

    add 로그인 파이어베이스.유효성검사, 페이스북png

--- projectwalktogether/src/pages/LoginPage.tsx
+++ projectwalktogether/src/pages/LoginPage.tsx
@@ -1,40 +1,102 @@
 import React from "react";
 import styled from "styled-components";
-
+import { useState } from "react";
+import { signInWithEmailAndPassword, setPersistence, browserSessionPersistence, getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { authService } from "../common/firebase";
+import { emailRegex, pwdRegex } from "../utils/UserInfoRegex";
+import { useNavigate } from "react-router-dom";
+import facebookImg from "../assets/facebook.png";
 const LoginPage = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  //onchange로 값을 저장.
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmitClick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setPersistence(authService, browserSessionPersistence)
+      .then(() => {
+        return signInWithEmailAndPassword(authService, email, password)
+          .then(() => {
+            navigate("/", { replace: true });
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            alert("로그인 실패");
+            if (email.length === 0) {
+              alert("이메일을 입력해 주세요");
+            } else if (emailRegex.test(email) === false) {
+              alert("이메일을 정확히 입력해 주세요");
+            } else if (password.length === 0) {
+              alert("비밀번호를 입력해 주세요");
+            } else if (pwdRegex.test(password) === false) {
+              alert("비밀번호를 정확히 입력해 주세요 ");
+            }
+          });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
+
+  //firebase google 로그인
+
   return (
-    <div>
-      <form>
-        <InputBox>
+    <InputBox>
+      <form onSubmit={handleSubmitClick}>
+        <>
           <LoginLogo>
             <h1>같이걸을래?</h1>
           </LoginLogo>
 
           <InputBoxContent>
             <Inputholder>
-              <Input name="아이디" value="아이디" />
+              <Input placeholder="이메일" onChange={onChangeEmail} />
             </Inputholder>
             <Inputholder>
-              <Input name="비밀번호" value="비밀번호" />
+              <Input placeholder="비밀번호" onChange={onChangePassword} />
             </Inputholder>
           </InputBoxContent>
           <ButtonBox>
-            <RegisterBtn>회원 가입</RegisterBtn>
-            <LoginBtn>로그인</LoginBtn>
+            <RegisterBtn onClick={() => navigate("/signup")}>회원 가입</RegisterBtn>
+            <LoginBtn type="submit">로그인</LoginBtn>
           </ButtonBox>
-        </InputBox>
+        </>
       </form>
-    </div>
+      <SocialBtnBox>
+        <Facebook src={facebookImg} />
+      </SocialBtnBox>
+    </InputBox>
   );
 };
 export default LoginPage;
 const InputBox = styled.div`
   position: relative;
-  width: 26.875rem;
-  height: 60rem;
+  width: 22.5rem;
+  height: 40rem;
+
+  margin-left: -11.25rem;
+  margin-top: 5rem;
+
+  left: 50%;
   border-radius: 1.875rem;
   padding: 0.3125rem;
-  top: 9.375rem;
+  top: 3.125rem;
   border: 0.125rem solid #2192ff;
   z-index: 1;
 `;
@@ -42,7 +104,7 @@ const InputBox = styled.div`
 // 인풋태그
 const Input = styled.input`
   border: none;
-  width: 18.75rem;
+  width: 16rem;
   height: 2.375rem;
   position: relative;
   left: 1.875rem;
@@ -50,24 +112,24 @@ const Input = styled.input`
 `;
 
 const Inputholder = styled.div`
-  border-radius: 30px;
-  width: 380px;
-  height: 45px;
-  border: 3px solid #b2c8df;
+  border-radius: 1.875rem;
+  width: 19rem;
+  height: 2.8125rem;
+  border: 0.1875rem solid #b2c8df;
   color: #b2c8df;
-  margin-top: 25px;
+  margin-top: 1.5625rem;
 `;
 
 //인풋을 둘러싼 박스
 const InputBoxContent = styled.div`
-  margin: 1.25rem;
+  margin: 20px;
 `;
 
 const LoginLogo = styled.div`
   text-align: center;
-  margin-top: 6.875rem;
-  margin-bottom: 1.5625rem;
-  font-size: 1.25rem;
+  margin-top: 110px;
+  margin-bottom: 25px;
+  font-size: 20px;
   color: #2192ff;
 `;
 
@@ -78,14 +140,14 @@ const ButtonBox = styled.div`
 `;
 
 const LoginBtn = styled.button`
-  border-radius: 2.5rem;
-  width: 6.875rem;
-  height: 40px;
-  font-size: 20px;
+  border-radius: 40px;
+  width: 110px;
+  height: 2.5rem;
+  font-size: 1.25rem;
   color: #2192ff;
   border-color: #2192ff;
   font-weight: 900;
-  margin-top: 0.9375rem;
+  margin-top: 15px;
   background-color: White;
   &:hover {
     background: cornflowerblue;
@@ -95,14 +157,27 @@ const LoginBtn = styled.button`
 `;
 
 const RegisterBtn = styled.button`
-  width: 110px;
-  height: 40px;
+  width: 6.875rem;
+  height: 2.5rem;
   color: #2192ff;
   border: none;
-  margin-top: 10px;
+  margin-top: 0.625rem;
   background-color: White;
   &:hover {
     color: #816ceb;
     transition: 0.4s;
   }
+`;
+
+const SocialBtnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Facebook = styled.img`
+  text-align: center;
+  margin-top: 60px;
+  margin-bottom: 25px;
+  width: 50px;
+  color: #2192ff;
 `;
