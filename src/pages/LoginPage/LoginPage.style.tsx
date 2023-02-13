33ed92f1d8b8bd754f9eadde40c954aabffd14ce
Leekee01 <zeje23@naver.com>
2023-02-13 16:20:52 +0900

    [feat]소셜로그인 구글.페이스북

---
+++ src/pages/LoginPage/LoginPage.style.tsx
@@ -0,0 +1,156 @@
+import styled from 'styled-components';
+export const InputBox = styled.div`
+  position: relative;
+  width: 22.5rem;
+  height: 40rem;
+  margin-left: -11.25rem;
+  margin-top: 5rem;
+  left: 50%;
+  border-radius: 1.875rem;
+  padding: 0.3125rem;
+  top: 3.125rem;
+  border: 0.125rem solid #2192ff;
+  z-index: 1;
+`;
+// 인풋태그
+export const Input = styled.input`
+  border: none;
+  width: 250px;
+  height: 38px;
+  position: relative;
+  left: 30px;
+  outline: none;
+  :focus-visible {
+    outline: none;
+  }
+`;
+
+//Input태그의 테두리
+export const Inputholder = styled.div`
+  border-radius: 10px;
+  width: 300px;
+  height: 45px;
+  border: 3px solid #b2c8df;
+  color: #b2c8df;
+  margin-top: 25px;
+`;
+
+//인풋을 둘러싼 박스
+export const InputBoxContent = styled.div`
+  margin: 20px;
+`;
+
+//잠깐만 !
+export const LoginLogo = styled.div`
+  text-align: center;
+  margin-top: 110px;
+  margin-bottom: 25px;
+  font-size: 20px;
+  color: #2192ff;
+`;
+
+export const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Line = styled.div`
+  border-top: 1px solid #444444;
+  margin-top: 5px;
+  margin-bottom: 3px;
+  width: 100px;
+  margin: auto;
+`;
+
+export const SocialBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 50px;
+`;
+
+export const FindBox = styled.div`
+  display: flex;
+  margin-top: 10px;
+`;
+export const LineBox = styled.div`
+  align-items: center;
+  display: flex;
+`;
+export const Facebook = styled.img`
+  margin: 20px;
+  width: 40px;
+  heigh: 40px;
+`;
+export const Google = styled.img`
+  margin: 20px;
+  width: 40px;
+  heigh: 40px;
+`;
+export const Twitter = styled.img`
+  margin: 20px;
+  width: 40px;
+  heigh: 40px;
+`;
+
+export const LoginBtn = styled.button`
+  border-radius: 40px;
+  width: 250px;
+  height: 40px;
+  font-size: 20px;
+  color: #2192ff;
+  border-color: #2192ff;
+  font-weight: 900;
+  margin-top: 15px;
+  background-color: White;
+  cursor: pointer;
+  &:hover {
+    background: cornflowerblue;
+    color: white;
+    transition: 0.5s;
+  }
+`;
+export const ThirdBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const RegisterBtn = styled.button`
+  width: 110px;
+  height: 40px;
+  color: #2192ff;
+  border: none;
+  margin-top: 10px;
+  background-color: White;
+  cursor: pointer;
+  &:hover {
+    color: #816ceb;
+    transition: 0.4s;
+  }
+`;
+
+export const Findid = styled.div`
+  color: #2192ff;
+  border: none;
+
+  margin-right: 5px;
+  background-color: White;
+  cursor: pointer;
+  &:hover {
+    color: #816ceb;
+    transition: 0.4s;
+  }
+`;
+export const FindPasswd = styled.div`
+  color: #2192ff;
+  border: none;
+
+  margin-left: 5px;
+  background-color: White;
+  cursor: pointer;
+  &:hover {
+    color: #816ceb;
+    transition: 0.4s;
+  }
+`;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,28 +1,148 @@
-import React, { useEffect, useState } from 'react';
-import { login, logout, onUserStateChange } from '../../common/firebase';
+import React from 'react';
+import * as S from './LoginPage.style';
+
+import { useState } from 'react';
+import { signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, TwitterAuthProvider, FacebookAuthProvider, setPersistence, browserSessionPersistence } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+import { authService } from '../../common/firebase';
+import { emailRegex, pwdRegex } from '../../utils/UserInfoRegex';
 
 const LoginPage = () => {
-  // 임시방면
-  const [user, setUser] = useState<any>();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginModalopen, setLoginModalopen] = useState(false);
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    onUserStateChange((user: any) => {
-      console.log(user);
-      setUser(user);
-    });
-  }, []);
-  const handleLogin = () => {
-    login().then(setUser);
+  //onchange로 값을 저장.
+  const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
-  const handleLogout = () => {
-    logout().then(setUser);
+  const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  //firebase
+  const handleSubmitClick = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setPersistence(authService, browserSessionPersistence)
+      .then(() => {
+        return signInWithEmailAndPassword(authService, email, password)
+          .then(() => {
+            navigate('/', { replace: true });
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            alert('로그인 실패');
+            if (email.length === 0) {
+              alert('이메일을 입력해 주세요');
+            } else if (emailRegex.test(email) === false) {
+              alert('이메일을 정확히 입력해 주세요');
+            } else if (password.length === 0) {
+              alert('비밀번호를 입력해 주세요');
+            } else if (pwdRegex.test(password) === false) {
+              alert('비밀번호를 정확히 입력해 주세요 ');
+            }
+          });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        alert(errorMessage);
+      });
+  };
+
+  //비밀번호 찾기
+  const findPwd = (e: any) => {
+    e.preventDefault();
+    setLoginModalopen(true);
+  };
+
+  //소셜로그인 페이스북
+
+  const signInWithFacebook = () => {
+    const provider = new FacebookAuthProvider();
+    signInWithPopup(authService, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate('/', { replace: true });
+        return user;
+      })
+      .catch(console.error);
+  };
+
+  //소셜로그인 구글
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(authService, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate('/', { replace: true });
+        return user;
+      })
+      .catch(console.error);
+  };
+
+  //트위터 로그인
+  const signInWithTwitter = () => {
+    const provider = new TwitterAuthProvider();
+    signInWithPopup(authService, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        navigate('/', { replace: true });
+        return user;
+      })
+      .catch(console.error);
   };
 
   return (
-    <>
-      {!user && <button onClick={handleLogin}>Login</button>}
-      {user && <button onClick={handleLogout}>Logout</button>}
-    </>
+    <S.InputBox>
+      <form onSubmit={handleSubmitClick}>
+        <div>
+          <S.LoginLogo>
+            <h1>같이 걸을래?</h1>
+          </S.LoginLogo>
+
+          <S.InputBoxContent>
+            <S.Inputholder>
+              <S.Input type="email" name="아이디" placeholder="이메일을 입력해주세요" onChange={onChangeEmail}></S.Input>
+            </S.Inputholder>
+            <S.Inputholder>
+              <S.Input type="password" name="비밀번호" placeholder="비밀번호를 입력해주세요" onChange={onChangePassword}></S.Input>
+            </S.Inputholder>
+          </S.InputBoxContent>
+          <S.ButtonBox>
+            <S.LoginBtn type="submit">로그인</S.LoginBtn>
+          </S.ButtonBox>
+          <S.LineBox>
+            <S.Line />
+            <text>또는</text>
+            <S.Line />
+          </S.LineBox>
+
+          <S.SocialBox>
+            <S.Facebook onClick={signInWithFacebook} src="/assets/facebook.png" />
+            <S.Google onClick={signInWithGoogle} src="assets/google.png" />
+            <S.Twitter onClick={signInWithTwitter} src="assets/twitter.png" />
+          </S.SocialBox>
+          <S.ThirdBox>
+            <S.RegisterBtn type="button" onClick={() => navigate('/signup')}>
+              회원 가입
+            </S.RegisterBtn>
+            <S.FindBox>
+              <S.Findid>계정찾기</S.Findid>|<S.FindPasswd>비밀번호찾기</S.FindPasswd>
+            </S.FindBox>
+          </S.ThirdBox>
+        </div>
+      </form>
+    </S.InputBox>
   );
 };
+
 export default LoginPage;
