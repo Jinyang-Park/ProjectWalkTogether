0ca7f1dcfd02fcfdcf673dbe73cdd3c15751b346
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-16 01:03:48 +0900

    [feats] : 사용자 위치를 지도의 중심으로 가져오기 기능

--- src/pages/MapPage/Map/map.tsx
+++ src/pages/MapPage/Map/map.tsx
@@ -8,17 +8,44 @@ import { useRecoilState, useRecoilValue } from 'recoil'
 
 import * as S from '../Map/map.style'
 
-import { Map, MapMarker } from 'react-kakao-maps-sdk'
+import {
+  Map,
+  MapMarker,
+  ZoomControl,
+  MapTypeControl,
+} from 'react-kakao-maps-sdk'
 
 const MapContainer = () => {
+  const [myLoca, setMyLoca] = useState({ lat: 36.5, lng: 127.8 })
+  React.useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어온다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setMyLoca({
+            lat: position.coords.latitude, // 위도
+            lng: position.coords.longitude, // 경도
+          })
+        },
+        (err) => {
+          alert('현재 위치를 표시할 수 없어요')
+        },
+        { enableHighAccuracy: true } // 위치정보의 정확도를 높이는 옵션
+      )
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때
+      alert('현재 위치를 표시할 수 없어요')
+    }
+  }, [])
+
   return (
-    <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: '100%', height: '360px' }}
-    >
+    <Map center={myLoca} style={{ width: '100%', height: '100%' }}>
       <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
         <div style={{ color: '#000' }}>Hello World!</div>
       </MapMarker>
+
+      <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+      <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
     </Map>
   )
 }
