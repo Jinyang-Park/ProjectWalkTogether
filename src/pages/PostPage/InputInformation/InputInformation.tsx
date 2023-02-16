903182457a157ab813581f07bf5b73ea19196701
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-16 15:01:32 +0900

    [feat] : 주소창 검색 기능 활성화

--- src/pages/PostPage/InputInformation/InputInformation.tsx
+++ src/pages/PostPage/InputInformation/InputInformation.tsx
@@ -20,6 +20,18 @@ function InputInformation() {
   // 지도 좌표를 저장할 state
   const [position, setPosition] = useState({ lat: 36.5, lng: 127.8 })
 
+  // 키워드로 장소검색하기를 위한 state
+  const [info, setInfo] = useState<any>()
+  const [markers, setMarkers] = useState([])
+  const [map, setMap] = useState<any>()
+  const [bounds, setBounds] = useState()
+
+  // input value 를 가져오기 위한 state
+  const [search, setSearch] = useState('')
+  const onChange = (e) => {
+    setSearch(e.target.value)
+  }
+
   // 사용자 위치를 가져오기 위한 useEffect
   React.useEffect(() => {
     if (navigator.geolocation) {
@@ -42,8 +54,44 @@ function InputInformation() {
     }
   }, [])
 
-  // 주소-좌표 변환 객체를 생성합니다
-  var geocoder = new kakao.maps.services.Geocoder()
+  // 키워드로 장소검색하기 위한 useEffect
+  useEffect(() => {
+    SearchFunction()
+  }, [map])
+
+  //  // 지도를 불러오기 위한 함수
+  const SearchFunction = () => {
+    if (!map) return
+    const ps = new kakao.maps.services.Places()
+
+    ps.keywordSearch(search, (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        const bounds = new kakao.maps.LatLngBounds()
+        let markers = []
+
+        for (var i = 0; i < data.length; i++) {
+          // @ts-ignore
+          // markers.push({
+          //   position: {
+          //     lat: data[i].y,
+          //     lng: data[i].x,
+          //   },
+          //   content: data[i].place_name,
+          // })
+          // @ts-ignore
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x))
+        }
+        setMarkers(markers)
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds)
+      }
+    })
+  }
+
+  console.log(search)
 
   return (
     <S.MapNInputBox>
@@ -55,16 +103,29 @@ function InputInformation() {
           <Map
             center={myLoca}
             style={{ width: '100%', height: '100%' }}
+            level={3}
             onClick={(_t, mouseEvent) =>
               setPosition({
                 lat: mouseEvent.latLng.getLat(),
                 lng: mouseEvent.latLng.getLng(),
               })
             }
+            onCreate={setMap}
           >
             <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
             <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
             {position && <MapMarker position={position} />}
+            {/* {markers.map((marker) => (
+              <MapMarker
+                key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+                position={marker.position}
+                onClick={() => setInfo(marker)}
+              >
+                {info && info.content === marker.content && (
+                  <div style={{ color: '#000' }}>{marker.content}</div>
+                )}
+              </MapMarker>
+            ))} */}
           </Map>
           {position && (
             <p>
@@ -78,8 +139,18 @@ function InputInformation() {
         </S.KakaoMap>
       </S.MapBox>
       <S.InputBox>
-        <S.InputAdressBox>
-          <input />
+        <S.InputAdressBox
+          onSubmit={(e) => {
+            e.preventDefault()
+            SearchFunction()
+          }}
+        >
+          <input
+            type={'text'}
+            placeholder={'주소를 입력해주세요'}
+            value={search}
+            onChange={onChange}
+          />
         </S.InputAdressBox>
         <S.InpuDayBox>
           <BasicDatePicker />
