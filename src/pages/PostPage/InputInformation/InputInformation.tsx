95ffe4fb23b2b384ebd2b7280835a9ee98dde551
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-16 14:00:44 +0900

    [feat] : inpurinformation.tsx 에 지도 삽입

--- src/pages/PostPage/InputInformation/InputInformation.tsx
+++ src/pages/PostPage/InputInformation/InputInformation.tsx
@@ -1,16 +1,81 @@
-import * as S from './InputInformation.style';
-import MaterialUIPickers from '../Hooks/Calendar/MuiCalendar';
-import BasicDatePicker from '../Hooks/Calendar/MuiDate';
+import * as S from './InputInformation.style'
+import MaterialUIPickers from '../Hooks/Calendar/MuiCalendar'
+import BasicDatePicker from '../Hooks/Calendar/MuiDate'
 // import MapContainer from '../../MapPage/Map/map';
-import MuiTime from '../Hooks/Calendar/MuiTime';
+import MuiTime from '../Hooks/Calendar/MuiTime'
+
+import {
+  Map,
+  MapMarker,
+  ZoomControl,
+  MapTypeControl,
+} from 'react-kakao-maps-sdk'
+import { useState, useEffect } from 'react'
+import React from 'react'
 
 function InputInformation() {
+  // 현재 위치를 가져오기 위한 state 생성
+  const [myLoca, setMyLoca] = useState({ lat: 36.5, lng: 127.8 })
+
+  // 지도 좌표를 저장할 state
+  const [position, setPosition] = useState({ lat: 36.5, lng: 127.8 })
+
+  // 사용자 위치를 가져오기 위한 useEffect
+  React.useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어온다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setMyLoca({
+            lat: position.coords.latitude, // 위도
+            lng: position.coords.longitude, // 경도
+          })
+        },
+        (err) => {
+          alert('현재 위치를 표시할 수 없어요')
+        },
+        { enableHighAccuracy: true } // 위치정보의 정확도를 높이는 옵션
+      )
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때
+      alert('현재 위치를 표시할 수 없어요')
+    }
+  }, [])
+
+  // 주소-좌표 변환 객체를 생성합니다
+  var geocoder = new kakao.maps.services.Geocoder()
+
   return (
     <S.MapNInputBox>
       PostPageInputinfomation
       <S.MapBox>
         안녕하세요
-        <S.KakaoMap>{/* <MapContainer /> */}</S.KakaoMap>
+        <S.KakaoMap>
+          {' '}
+          <Map
+            center={myLoca}
+            style={{ width: '100%', height: '100%' }}
+            onClick={(_t, mouseEvent) =>
+              setPosition({
+                lat: mouseEvent.latLng.getLat(),
+                lng: mouseEvent.latLng.getLng(),
+              })
+            }
+          >
+            <ZoomControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+            <MapTypeControl position={kakao.maps.ControlPosition.TOPRIGHT} />
+            {position && <MapMarker position={position} />}
+          </Map>
+          {position && (
+            <p>
+              {'클릭한 위치의 위도는 ' +
+                position.lat +
+                ' 이고, 경도는 ' +
+                position.lng +
+                ' 입니다'}
+            </p>
+          )}
+        </S.KakaoMap>
       </S.MapBox>
       <S.InputBox>
         <S.InputAdressBox>
@@ -27,7 +92,7 @@ function InputInformation() {
         </S.InputTimeBox>
       </S.InputBox>
     </S.MapNInputBox>
-  );
+  )
 }
 
-export default InputInformation;
+export default InputInformation
--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -1,57 +1,57 @@
-import React, { useEffect } from 'react';
-import { useState } from 'react';
-import { Time, TitleInput } from './Hooks/Rocoil/Atom';
-import { useRecoilValue } from 'recoil';
-import { getAuth } from 'firebase/auth';
-import { uuidv4 } from '@firebase/util';
-import { collection, addDoc } from 'firebase/firestore';
-import { dbService } from '../../common/firebase';
-import Mainpost from './MainPost/Mainpost';
-import IuputInformation from './InputInformation/InputInformation';
-import * as S from './Postpage.style';
-import CommonStyles from './../../styles/CommonStyles';
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { Time, TitleInput } from './Hooks/Rocoil/Atom'
+import { useRecoilValue } from 'recoil'
+import { getAuth } from 'firebase/auth'
+import { uuidv4 } from '@firebase/util'
+import { collection, addDoc } from 'firebase/firestore'
+import { dbService } from '../../common/firebase'
+import MainPost from './Mainpost/Mainpost'
+import IuputInformation from './InputInformation/InputInformation'
+import * as S from './Postpage.style'
+import CommonStyles from './../../styles/CommonStyles'
 
 const PostPage = () => {
-  const [loginModalopen, setLoginModalopen] = useState(false); //아이디 찾기 모달창
-  const [postDb, setPostDb] = useState({}); //파이어베이스DB
-  const [postHour, setPostHour] = useState(''); //약속 시간.날짜
-  const [postMinut, setPostMinute] = useState(''); //약속 시간.시각
-  const [postTime, setPostTime] = useState(''); //작성시간
-  const [postLatitude, setPostLtitude] = useState(''); //위도
-  const [postLongitude, setPostLongitude] = useState(''); //경도
-  const [postNowLatitude, setPostNowLtitude] = useState(''); //현재 위도
-  const [postNowLongitude, setPostNowLongitude] = useState(''); //현재 경도
-  const [postLiked, setPostLiked] = useState(false); //좋아요 여부
-  const [postCountLiked, setPostCountLiked] = useState(''); //좋아요 갯수
-  const [proceedState, setProceedState] = useState(''); //게시글의 진행사항
-  const [keyForChat, setKeyForChat] = useState(''); //채팅을 위해 생성한 id
-  const [postId, setPostId] = useState(''); //포스팅 id 고유값
-  const [postAuthor, setPostAuthor] = useState(''); //사용자 파이어베이스 uid
-  const [postNickname, setPostNickname] = useState(''); //사용자 닉네임 => 회원가입시시에 저장해 주거나 로컬에 저장하는 방법을 찾아야될 것 같다.
-  const [postAddress, setPostAddress] = useState(''); //만날 위치 시,군,구,단
-  const [postCategory, setPostCategory] = useState(''); //카테고리
+  const [loginModalopen, setLoginModalopen] = useState(false) //아이디 찾기 모달창
+  const [postDb, setPostDb] = useState({}) //파이어베이스DB
+  const [postHour, setPostHour] = useState('') //약속 시간.날짜
+  const [postMinut, setPostMinute] = useState('') //약속 시간.시각
+  const [postTime, setPostTime] = useState('') //작성시간
+  const [postLatitude, setPostLtitude] = useState('') //위도
+  const [postLongitude, setPostLongitude] = useState('') //경도
+  const [postNowLatitude, setPostNowLtitude] = useState('') //현재 위도
+  const [postNowLongitude, setPostNowLongitude] = useState('') //현재 경도
+  const [postLiked, setPostLiked] = useState(false) //좋아요 여부
+  const [postCountLiked, setPostCountLiked] = useState('') //좋아요 갯수
+  const [proceedState, setProceedState] = useState('') //게시글의 진행사항
+  const [keyForChat, setKeyForChat] = useState('') //채팅을 위해 생성한 id
+  const [postId, setPostId] = useState('') //포스팅 id 고유값
+  const [postAuthor, setPostAuthor] = useState('') //사용자 파이어베이스 uid
+  const [postNickname, setPostNickname] = useState('') //사용자 닉네임 => 회원가입시시에 저장해 주거나 로컬에 저장하는 방법을 찾아야될 것 같다.
+  const [postAddress, setPostAddress] = useState('') //만날 위치 시,군,구,단
+  const [postCategory, setPostCategory] = useState('') //카테고리
 
   //약속 시간
-  const meetTime = useRecoilValue(Time);
-  const meetTimeObectToString = JSON.stringify(Object.values(meetTime)[2]);
-  const OTS = Object.values(meetTime)[2].toString();
-  const weeks = OTS.slice(0, 3);
-  var toayweek = '';
+  const meetTime = useRecoilValue(Time)
+  const meetTimeObectToString = JSON.stringify(Object.values(meetTime)[2])
+  const OTS = Object.values(meetTime)[2].toString()
+  const weeks = OTS.slice(0, 3)
+  var toayweek = ''
   switch (weeks) {
     case 'Sun':
-      toayweek = '일';
-      break;
+      toayweek = '일'
+      break
     case 'Tue':
-      toayweek = '화';
-      break;
+      toayweek = '화'
+      break
   }
 
-  const meetTimeValue = Object.values(meetTime);
-  const weekValue = meetTimeObectToString.slice(1, 9);
+  const meetTimeValue = Object.values(meetTime)
+  const weekValue = meetTimeObectToString.slice(1, 9)
   // const meetHour = meetTimeObectToString.slice(12, 19); //시간 이상하게나옴
-  const meetYearMonth = meetTimeObectToString.slice(1, 9); //년월
-  const meetDay = meetTimeObectToString.slice(9, 11); //일
-  const week = meetTimeObectToString;
+  const meetYearMonth = meetTimeObectToString.slice(1, 9) //년월
+  const meetDay = meetTimeObectToString.slice(9, 11) //일
+  const week = meetTimeObectToString
   // let meetDayHour = 0;
   // if (Number(meetTimeValue[8]) < 10) {
   //   const meetDayHourzero = meetTimeValue[8] + 'a';
@@ -59,39 +59,39 @@ const PostPage = () => {
   //   const meetDayHourzero = meetTimeValue[8];
   // }
   //시
-  const meetDayHour = meetTimeValue[8]; // am의경우 0이 앞에 안 붙는다.
-  const meetDayMinute = meetTimeObectToString.slice(14, 17); //분
-  let meeting = `${meetYearMonth}${meetDay}${meetDayHour}${meetDayMinute}` + ``;
+  const meetDayHour = meetTimeValue[8] // am의경우 0이 앞에 안 붙는다.
+  const meetDayMinute = meetTimeObectToString.slice(14, 17) //분
+  let meeting = `${meetYearMonth}${meetDay}${meetDayHour}${meetDayMinute}` + ``
 
-  const Title = useRecoilValue(TitleInput);
+  const Title = useRecoilValue(TitleInput)
 
   /////////
   //현재시간
   /////////
-  let today = new Date(); // today 객체에 Date()의 결과를 넣어줬다
+  let today = new Date() // today 객체에 Date()의 결과를 넣어줬다
   const time = {
     year: today.getFullYear(), //현재 년도
     month: today.getMonth() + 1, // 현재 월
     date: today.getDate(), // 현제 날짜
     hours: today.getHours(), //현재 시간
     minutes: today.getMinutes(), //현재 분
-  };
-  let timestring = `${time.year}/${time.month}/${time.date} ${time.hours}:${time.minutes}`;
+  }
+  let timestring = `${time.year}/${time.month}/${time.date} ${time.hours}:${time.minutes}`
 
-  const auth = getAuth();
-  const user = auth.currentUser?.uid;
-  const nickname = auth.currentUser?.displayName;
+  const auth = getAuth()
+  const user = auth.currentUser?.uid
+  const nickname = auth.currentUser?.displayName
 
   /////////////
   //콘솔확인용/
   ////////////
   useEffect(() => {
-    console.log('meetTime:', toayweek);
+    console.log('meetTime:', toayweek)
     // setPostTime(timestring); //현재 시간
     // setPostHour(meeting); //약속 시간
     // setPostNickname(nickname);
     // setPostAuthor(user);
-  });
+  })
 
   ////////////
   //작성완료//
@@ -106,25 +106,25 @@ const PostPage = () => {
         TimeStamp_Posting: postTime,
         Title_Posting: Title,
         UID: postAuthor,
-      });
-      console.log('Document written with ID: ', docRef.id);
-      alert('저장완료');
+      })
+      console.log('Document written with ID: ', docRef.id)
+      alert('저장완료')
     } catch (e) {
-      console.error('Error adding document: ', e);
+      console.error('Error adding document: ', e)
     }
-  };
+  }
 
   return (
     <CommonStyles>
       <S.Boxcontainer>
-        <Mainpost />
+        <MainPost />
         <IuputInformation />
         <S.PostSubmitBox>
           <S.PostSubmitBtn onClick={handleSubmit}>포스팅 하기</S.PostSubmitBtn>
         </S.PostSubmitBox>
       </S.Boxcontainer>
     </CommonStyles>
-  );
-};
+  )
+}
 
-export default PostPage;
+export default PostPage
