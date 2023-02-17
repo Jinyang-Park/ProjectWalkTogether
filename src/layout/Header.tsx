9832a94c5a9613b79f168999cd96b1dfc2366358
lth97 <0ne0rzer0@naver.com>
2023-02-17 19:27:00 +0900

    [fix] : src//hooks/useDetectClose 부분 타입오류 수정

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -24,22 +24,22 @@ const Header = () => {
       <S.Nav>
         <S.NavUl>
           <S.NavLi>
-            <S.NavText to="/">Home</S.NavText>
+            <S.NavText to='/'>Home</S.NavText>
           </S.NavLi>
           <S.NavLi>
-            <S.NavText to="/map">Map</S.NavText>
+            <S.NavText to='/map'>Map</S.NavText>
           </S.NavLi>
           <S.NavLi>
-            <S.NavText to="/chat">chat</S.NavText>
+            <S.NavText to='/chat'>chat</S.NavText>
           </S.NavLi>
           <S.NavLi>
-            <S.NavText to="/category">카테고리</S.NavText>
+            <S.NavText to='/category'>카테고리</S.NavText>
           </S.NavLi>
           <S.NavLi>
-            <S.NavText to="/postpage">글쓰기</S.NavText>
+            <S.NavText to='/postpage'>글쓰기</S.NavText>
           </S.NavLi>
           <S.NavLi>
-            <S.NavText to="/detailpage">상세</S.NavText>
+            <S.NavText to='/detailpage'>상세</S.NavText>
           </S.NavLi>
         </S.NavUl>
 
@@ -62,7 +62,7 @@ const Header = () => {
                       <KakaoLogoutButton />
                     </S.Li>
                     <S.Li>
-                      <S.NavText to="/postpage">글쓰기</S.NavText>
+                      <S.NavText to='/postpage'>글쓰기</S.NavText>
                     </S.Li>
                   </S.Ul>
                 </S.DropNav>
