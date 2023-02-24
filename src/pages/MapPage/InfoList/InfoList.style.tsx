f898654e21ac91dd6626b8089db6851f5ddb7704
MINSUNG KIM <78587041+GhostPines@users.noreply.github.com>
2023-02-24 11:29:08 +0900

    [feat] MapPage 마우스 커서 변환 및 네비게이터 연결

--- src/pages/MapPage/InfoList/InfoList.style.tsx
+++ src/pages/MapPage/InfoList/InfoList.style.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const HeaderLine = styled.span`
   display: flex;
@@ -15,14 +15,14 @@ export const HeaderLine = styled.span`
     justify-content: space-between;
     align-items: center;
   }
-`
+`;
 export const HeaderLineTitle = styled.span`
   font-size: 30px;
   font-weight: 600;
   @media screen {
     font-size: Large;
   }
-`
+`;
 export const HeaderLineCategory = styled.span`
   display: flex;
   justify-content: center;
@@ -35,20 +35,20 @@ export const HeaderLineCategory = styled.span`
     width: 100px;
     height: 30px;
   }
-`
+`;
 export const SearchLine = styled.span`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   padding: 10px;
-`
+`;
 export const SearchLineSearchByLocation = styled.span`
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: smaller;
-`
+`;
 export const SearchLineSearchInputBox = styled.span`
   width: 100%;
   height: 15px;
@@ -58,69 +58,70 @@ export const SearchLineSearchInputBox = styled.span`
   margin-bottom: 10px;
   padding: 10px;
   border: 1px solid black;
-`
+`;
 export const SearchLineTotalCount = styled.span`
   font-size: smaller;
   color: gray;
 
   margin-bottom: 30px;
-`
+`;
 export const ResultList = styled.span`
   width: 868px;
   display: grid;
   grid-template-rows: 1fr;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-`
+`;
 export const ResultListCard = styled.span`
   display: flex;
   flex-direction: column;
   grid-gap: 1px;
   margin: 20px;
-`
+  cursor: pointer;
+`;
 
 export const ResultListCardImage = styled.img`
   width: 100%;
   height: 96px;
   margin-bottom: 5px;
-`
+`;
 export const ResultListCardTitle = styled.span`
   width: 180px;
   height: 40px;
   font-size: 16px;
   font-weight: 500;
   margin-bottom: 5px;
-`
+`;
 
 export const ResultListCardLocationTimeDateWrapper = styled.span`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   width: 180px;
-`
+`;
 export const ResultListCardLocation = styled.span`
   font-size: smaller;
   color: coral;
   width: 180px;
-`
+`;
 export const ResultListCardLine = styled.span`
   width: 180px;
   height: 1px;
   background-color: gray;
-`
+`;
 export const ResultListCardDateTimeWrapper = styled.span`
   display: flex;
   flex-direction: row;
 
   justify-content: start;
-`
+`;
 export const ResultListCardDate = styled.span`
   font-size: smaller;
   width: 80px;
-`
+`;
 export const ResultListCardTime = styled.span`
   font-size: smaller;
   width: 80px;
-`
+`;
 export const ResultListTagList = styled.span`
   display: flex;
   flex-direction: row;
@@ -128,15 +129,15 @@ export const ResultListTagList = styled.span`
   gap: 5px;
   color: gray;
   margin-top: 5px;
-`
-export const ResultListTag = styled.span``
+`;
+export const ResultListTag = styled.span``;
 
 export const ResultListCardLike = styled.span`
   margin-left: 5px;
   color: red;
-`
+`;
 
 export const ResultListCardDateTimeLikeWrapper = styled.span`
   display: flex;
   flex-direction: row;
-`
+`;
