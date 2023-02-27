7f20c07a2f454f27a2721daaa4c3e64a63784680
rhsok823 <rhsok823@gmail.com>
2023-02-28 00:37:26 +0900

    [feat]쓰고 불러오기작업중

--- src/pages/ChatPage/ChattingBox/ChattingBox.tsx
+++ src/pages/ChatPage/ChattingBox/ChattingBox.tsx
@@ -1,8 +1,20 @@
 import React, { useEffect } from 'react';
 import * as S from './ChattingBox.style';
 import { useState } from 'react';
-import { authService, dbService } from '../../../common/firebase';
-import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
+import { dbService } from '../../../common/firebase';
+import {
+  setDoc,
+  doc,
+  serverTimestamp,
+  query,
+  collection,
+  where,
+  orderBy,
+  getDocs,
+  onSnapshot,
+  addDoc,
+} from 'firebase/firestore';
+
 import {
   chattingusers,
   currentUserUid,
@@ -12,20 +24,59 @@ import { useRecoilValue } from 'recoil';
 
 function ChattingBox() {
   const [message, setMessage] = useState('');
+  const [getmessage, setGetMessage] = useState<any>([]);
   const roomId = useRecoilValue(tochattingbox);
 
   const chattinguser = useRecoilValue(currentUserUid);
 
+  // const chattime = Date();
+
   //메세지 전송시 함수
   const sendMessage = async (event) => {
-    event.preventDefault();
+    // event.preventDefault();
     if (message.trim() === '') {
       alert('채팅을 입력해 주세요!');
       return;
     } else {
+      const docRef = await addDoc(collection(dbService, 'Chatting'), {
+        chattingRoomId: roomId,
+        message: message,
+        user: chattinguser,
+        createdAt: new Date(),
+      });
+
+      console.log('docRef:', docRef);
     }
   };
-  useEffect(() => {}, []);
+
+  const getChatting = async () => {
+    if (chattinguser === '') {
+      return;
+    }
+    const querySnapshot = await getDocs(
+      query(
+        collection(dbService, 'Chatting'),
+        where('chattingRoomId', '==', roomId)
+        // orderBy('createdAt', 'desc')
+      )
+    );
+    let list = [];
+    querySnapshot.forEach((doc) => {
+      list = [...list, { id: doc.id, ...doc.data() }];
+    });
+    setGetMessage(list);
+    console.log('list:', list);
+  };
+
+  useEffect(() => {
+    getChatting();
+  }, [roomId]);
+
+  const nowmessage = getmessage;
+
+  console.log('chattime:');
+
+  console.log('nowmessage:', nowmessage);
 
   return (
     <div>
@@ -37,68 +88,21 @@ function ChattingBox() {
           <S.ChattingNicknameto>여기가 닉네임입니다.</S.ChattingNicknameto>
         </S.ChattingNickname>
         <S.ChattingContent>
-          {/* 글 들어가는 곳 */} <S.ChattingText> 안녕하세요 </S.ChattingText>
-          <S.ChattingText> 안녕하세요 </S.ChattingText>
-          <S.ChattingTextBox>
-            {/* <S.ChattingRightTime>10:50</S.ChattingRightTime> */}
-            <S.ChattingText>하위</S.ChattingText>
-          </S.ChattingTextBox>
-          <S.ChattingTextBox>
-            <S.ChattingText>
-              원래 위에서 아래로 내려오는 형식이었다(default) 카톡처럼 아래에서
-              위로 올라가는 ui를 만들고 싶었지만 아무리 구글링을 해도
-              안나오더라(아직 구글링이 미숙한탓..) 그냥 이걸 얘기하고
-              싶었다(한탄) .... 다른 사람에게 꼭 도움이 되는 코드이길 바라며..
-              아래에서 위로 올라가는 UI 채팅, 밑에서 위로 올라가는 UI 채팅
-              검색엔진에 걸리길 바라며..
-            </S.ChattingText>
-            <S.ChattingTime>10:50</S.ChattingTime>
-          </S.ChattingTextBox>
-          <S.ChattingTextBox>
-            <S.ChattingText>
-              원래 위에서 아래로 내려오는 형식이었다(default) 카톡처럼 아래에서
-              위로 올라가는 ui를 만들고 싶었지만 아무리 구글링을 해도
-              안나오더라(아직 구글링이 미숙한탓..) 그냥 이걸 얘기하고
-              싶었다(한탄) .... 다른 사람에게 꼭 도움이 되는 코드이길 바라며..
-              아래에서 위로 올라가는 UI 채팅, 밑에서 위로 올라가는 UI 채팅
-              검색엔진에 걸리길 바라며..
-            </S.ChattingText>
-            <S.ChattingTime>10:50</S.ChattingTime>
-          </S.ChattingTextBox>
-          <S.ChattingTextBox>
-            <S.ChattingText>
-              원래 위에서 아래로 내려오는 형식이었다(default) 카톡처럼 아래에서
-              위로 올라가는 ui를 만들고 싶었지만 아무리 구글링을 해도
-              안나오더라(아직 구글링이 미숙한탓..) 그냥 이걸 얘기하고
-              싶었다(한탄) .... 다른 사람에게 꼭 도움이 되는 코드이길 바라며..
-              아래에서 위로 올라가는 UI 채팅, 밑에서 위로 올라가는 UI 채팅
-              검색엔진에 걸리길 바라며..
-            </S.ChattingText>
-            <S.ChattingTime>10:50</S.ChattingTime>
-          </S.ChattingTextBox>{' '}
-          <S.ChattingTextBox>
-            <S.ChattingText>
-              원래 위에서 아래로 내려오는 형식이었다(default) 카톡처럼 아래에서
-              위로 올라가는 ui를 만들고 싶었지만 아무리 구글링을 해도
-              안나오더라(아직 구글링이 미숙한탓..) 그냥 이걸 얘기하고
-              싶었다(한탄) .... 다른 사람에게 꼭 도움이 되는 코드이길 바라며..
-              아래에서 위로 올라가는 UI 채팅, 밑에서 위로 올라가는 UI 채팅
-              검색엔진에 걸리길 바라며..
-            </S.ChattingText>
-            <S.ChattingTime>10:50</S.ChattingTime>
-          </S.ChattingTextBox>
-          <S.ChattingTextBoxLeft>
-            <S.ChattingImg></S.ChattingImg>
-            <S.ChattingTextLeft>
-              왜그래?왜그래왜그래왜그래왜그래왜그래왜그래왜그래왜그래왜그래래왜그래왜그래왜그래왜그래래왜그래왜그래왜그래왜그래래왜그래왜그래왜그래왜그래래왜그래왜그래왜그래왜그래
-            </S.ChattingTextLeft>
-            <S.ChattingTime>10:30</S.ChattingTime>
-          </S.ChattingTextBoxLeft>
-          <S.ChattingTextBoxLeft>
-            <S.ChattingImg></S.ChattingImg>
-            <S.ChattingTextLeft>잘 나오나? inlineblock</S.ChattingTextLeft>
-            <S.ChattingTime>10:30</S.ChattingTime>
-          </S.ChattingTextBoxLeft>
+          {/* 글 들어가는 곳 */}
+          {nowmessage.map((ars) => {
+            return ars.user === chattinguser ? (
+              <S.ChattingTextBox>
+                <S.ChattingText>{ars.message}</S.ChattingText>
+                <S.ChattingTime>10:50</S.ChattingTime>
+              </S.ChattingTextBox>
+            ) : (
+              <S.ChattingTextBoxLeft>
+                <S.ChattingImg></S.ChattingImg>
+                <S.ChattingTextLeft>잘 나오나? inlineblock</S.ChattingTextLeft>
+                <S.ChattingTime>10:30</S.ChattingTime>
+              </S.ChattingTextBoxLeft>
+            );
+          })}
         </S.ChattingContent>
         <S.ChattingInputBox>
           <form
