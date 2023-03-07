97e0e22a4c96b7740c65b27b6dd911b5badeb375
MJ Sung <themrsung@gmail.com>
2023-03-08 02:50:49 +0900

    최적화

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -21,6 +21,7 @@ import CardSection from '../../components/CardSection/CardSection';
 import Fire from '../../assets/Mainpage/Fire.svg';
 import Boog from '../../assets/Mainpage/boog.svg';
 import gitbal from '../../assets/Mainpage/gitbal.svg';
+import { Post, usePosts } from '../../api/getPosts';
 
 const Collection = (props: any) => {
   const navigate = useNavigate();
@@ -29,104 +30,45 @@ const Collection = (props: any) => {
   // 하나하나 객체를 불러서 카테고리리스트의 이미지와 name을 뿌려준다.
   // 라우터에서 카테고리 페이지를 만든다./categorypage/:category 이 부분이 :category 이 부분을 매개변수로 넘겨준다. /category/${Category.name} -> Category.name이 들어온다.
   // navigate사용해서 category/카테고리의 네임으로 이동되게 만들어야된다.
-  const [shoes, setShoes] = useState([]);
   const [title, setTitle] = useState('');
-  const [hot, setHot] = useState([]);
   const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    switch (id) {
+      case '1':
+        setTitle('신발신는 중');
+        setFile(<HotShoesImg src={Boog}></HotShoesImg>);
+        break;
+      case '2':
+        setTitle('뜨거운 신발');
+        setFile(<HotShoesImg src={Fire}></HotShoesImg>);
+        break;
+      case '3':
+        setTitle('매칭된 신발');
+        setFile(<HotShoesImg src={gitbal}></HotShoesImg>);
+        break;
+      default:
+        break;
+    }
+  }, []);
 
   const setParams = useSetRecoilState(paramsState);
 
-  useEffect(() => {
-    console.log(id);
-    if (id === '1') {
-      setTitle('신발신는 중');
-      setFile(<HotShoesImg src={Boog}></HotShoesImg>);
-      const q = query(
-        collection(dbService, 'Post'),
-        // Category_Posting이 파람스로 넘겨준 애들과 같은 애들만 뿌려줘라
-        orderBy('createdAt', 'desc')
-        // orderBy('TimeStamp_Posting', 'desc')
-      );
-      onSnapshot(q, (snapshot) => {
-        const getpostList = snapshot.docs.map((doc) => {
-          const postList = {
-            id: doc.id,
-            ...doc.data(),
-          };
-          return postList;
-        });
-        setShoes(getpostList);
-      });
-      console.log(shoes);
-    }
-    if (id === '2') {
-      setTitle('뜨거운 신발');
-      setFile(<HotShoesImg src={Fire}></HotShoesImg>);
-      const q = query(
-        collection(dbService, 'Post'),
-        // Category_Posting이 파람스로 넘겨준 애들과 같은 애들만 뿌려줘라
-        orderBy('createdAt', 'desc')
-      );
-      onSnapshot(q, (snapshot) => {
-        const getpostList = snapshot.docs.map((doc) => {
-          const postList = {
-            id: doc.id,
-            ...doc.data(),
-          };
-          return postList;
-        });
-        setShoes(getpostList);
-        console.log(getpostList);
-      });
+  // id
+  // 1 = 신발신는 중
+  // 2 = 뜨거운 신발
+  // 3 = 매칭된 신발
 
-      // return (
-      //   <S.LikedListItem>
-      //     {shoes
+  const shoes: Array<Post> = usePosts()
+    .filter((post) => {
+      if (id === '3') return post.ProceedState_Posting === 'postingDone';
+      else return post;
+    })
+    .sort((a, b) => {
+      if (id === '2') return b.LikedUsers.length - a.LikedUsers.length;
+      return 0;
+    });
 
-      //       .slice(0, 8)
-      //       .sort((a, b) => b.LikedUsers.length - a.LikedUsers.length)
-      //       .map((post: any) => {
-      //         return <CardSection key={post.id} post={post} />;
-      //       })}
-      //   </S.LikedListItem>
-      // );
-    }
-
-    if (id === '3') {
-      setTitle('매칭된 신발');
-      setFile(<HotShoesImg src={gitbal}></HotShoesImg>);
-      const q = query(
-        collection(dbService, 'Post'),
-
-        orderBy('createdAt', 'desc'),
-        where('ProceedState_Posting', '==', 'postingDone')
-      );
-      onSnapshot(q, (snapshot) => {
-        const getpostList = snapshot.docs.map((doc) => {
-          const postList = {
-            id: doc.id,
-            ...doc.data(),
-          };
-          return postList;
-        });
-        setShoes(getpostList);
-      });
-
-      console.log(shoes);
-    }
-    //state 하나로, value값에 따라서 state가 변경되게
-  }, [id]);
-
-  useEffect(() => {
-    if (!!shoes.length) {
-      if (id === '2') {
-        setHot(
-          shoes?.sort((a, b) => b.LikedUsers.length - a.LikedUsers.length)
-        );
-      } else setHot(shoes);
-    }
-  }, [shoes]);
-  console.log(title);
   return (
     <>
       <CategoryWrapper>
@@ -140,7 +82,7 @@ const Collection = (props: any) => {
         </S.CategoryTitleWrapper>
 
         <S.LikedListItem>
-          {hot?.map((post: any) => {
+          {shoes.map((post: any) => {
             return <CardSection key={post.id} post={post} />;
           })}
         </S.LikedListItem>
--- src/pages/MyPage/likepage/MyPageLike.tsx
+++ src/pages/MyPage/likepage/MyPageLike.tsx
@@ -1,25 +1,12 @@
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import { dbService } from '../../../common/firebase';
+import { Post, usePosts } from '../../../api/getPosts';
 import CardSection from '../../../components/CardSection/CardSection';
 import * as S from './MypageLike.style';
 
 const MyPageLike = (props: { uid: string }) => {
   const { uid } = props;
-  const [posts, setPosts] = useState([]);
-  const fetchPosts = async () => {
-    const querySnapshot = await getDocs(
-      query(
-        collection(dbService, 'Post'),
-        where('LikedUsers', 'array-contains', uid)
-      )
-    );
-    setPosts(querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-  };
-  useEffect(() => {
-    fetchPosts();
-  }, [posts]);
+  const posts: Array<Post> = usePosts().filter((post) =>
+    post.LikedUsers.includes(uid)
+  );
 
   return (
     <S.MyPageWriteWrap>
