fc850727b4baea723de617654bbae42f3166603b
Leekee01 <104826062+Leekee01@users.noreply.github.com>
2023-03-11 15:28:07 +0900

    Add files via upload

---
+++ src/common/firebase.ts
@@ -0,0 +1,29 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+
+export const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_APP_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const dbService = getFirestore(app);
+export const authService = getAuth(app);
+export const storage = getStorage(app);
+export const provider = new GoogleAuthProvider();
+export const apiKey = firebaseConfig.apiKey;
