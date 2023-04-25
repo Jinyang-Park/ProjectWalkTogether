d859d1c9cc6bbcdafabf4e1ca18443c3da3fb6f6
Jinyang-park <dbsskdud60@gmail.com>
2023-04-25 22:49:17 +0900

    [doc] 카테고리 반응형 완성성

--- src/pages/Category/Category.style.tsx
+++ src/pages/Category/Category.style.tsx
@@ -7,12 +7,16 @@ export const CategoryWrapper = styled.div`
   min-height: 60vh;
   margin: auto;
   margin-bottom: 80px;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const CategoryTitleWrapper = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   margin-top: 84px;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const CategoryTitle = styled.p`
   font-family: 'SUITERegular';
@@ -20,27 +24,46 @@ export const CategoryTitle = styled.p`
   font-weight: 500;
   font-size: 36px;
   line-height: 45px;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const CategoryImg = styled.img`
   width: 64px;
   height: 64px;
   border-radius: 4px;
+  @media screen and (max-width: 430px) {
+    margin-left: 20px;
+  }
 `;
 export const LikedListItem = styled.div`
   display: flex;
   flex-wrap: wrap;
   grid-gap: 70px 49.3px;
   margin-top: 46px;
+  @media screen and (max-width: 430px) {
+    width: calc(50% - 6px);
+    grid-gap: 70px 30px;
+    margin-left: 20px;
+  }
 `;
 export const FilterArea = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  @media screen and (max-width: 430px) {
+    margin: 0px 20px;
+    align-items: center;
+    align-content: center;
+    width: calc(50% - 50px);
+  }
 `;
 export const CategoryFilter = styled.div`
   display: flex;
   align-items: center;
+  @media screen and (max-width: 430px) {
+  }
 `;
+
 export const CategoryFilterWarpper = styled.div`
   display: flex;
   padding: 0px 10px 0px 10px;
@@ -52,6 +75,8 @@ export const CategoryFilterWarpper = styled.div`
   border-radius: 4px;
   margin-right: 8px;
   margin-top: 10px;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const FilterCategory = styled.p`
   width: auto;
@@ -61,15 +86,22 @@ export const FilterCategory = styled.p`
   line-height: 15px;
   color: #a6a6a6;
   margin-right: 4px;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const FilterCalendarIcon = styled.img`
   width: 10px;
   height: 10px;
   object-fit: contain;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const FilterSortWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  @media screen and (max-width: 430px) {
+    margin-top: 10px;
+  }
 `;
 export const FilterNewest = styled.p`
   font-family: 'SUITERegular';
@@ -84,6 +116,8 @@ export const FilterNewest = styled.p`
     cursor: pointer;
     font-weight: 600;
   }
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const FilterAreaLine = styled.span`
   margin: 1px 8px 0;
@@ -91,6 +125,8 @@ export const FilterAreaLine = styled.span`
   height: 16px;
   background-color: #e7e3e2;
   display: inline-block;
+  @media screen and (max-width: 430px) {
+  }
 `;
 export const NoResult = styled.div``;
 
@@ -101,4 +137,6 @@ export const NoResultTitle = styled.p`
   font-size: 16px;
   line-height: 45px;
   /* color: #7d8bae; */
+  @media screen and (max-width: 430px) {
+  }
 `;
